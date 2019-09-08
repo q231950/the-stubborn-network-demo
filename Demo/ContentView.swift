85d49654f43af2a5c6819b9be3da6ad4a423e8a9
Martin Kim Dund-Pham <martinkim.pham@gmail.com>
2019-09-08 19:39:37 +0200

    Initial commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Martin Kim Dung-Pham on 08.09.19.
+//  Copyright © 2019 Martin Kim Dung-Pham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
